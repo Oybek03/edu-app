cd31dee31ded5ea9f93dd96059d46032f06e458e
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-26 22:05:27 +0500

    teach ..

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -7,13 +7,13 @@ const Teachers = () => {
       <div className="teach-div">
         <div className="teach-card">
           <img
-            src="
-    
-    "
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
             alt="teach"
           />
-          <h2>Vevlet Vachon</h2>
-          <h3>Design Head</h3>
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
         </div>
       </div>
     </div>
