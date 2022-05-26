c68bbd48c66993b2b742310ad8e2e9852f18d214
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-26 13:14:17 +0500

    two section finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
-
-
 import React from "react";
+import Education from "./Education";
+import Feature from "./Feature";
 import Home from "./Home";
-
 
 class App extends React.Component {
   render() {
-    return <Home />
+    return (
+      <div>
+        {/* <Home /> */}
+        {/* <Education /> */}
+        <Feature />
+      </div>
+    );
   }
 }
 
