75f649f4398b2fd614f28bc12a5114b952da0160
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-26 23:08:56 +0500

    programs component end

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Programs = () => {
   return (
     <div>
-      <h1>Programs</h1>
+      <h1 className="prog-title">Programs</h1>
       <div className="big-prog-div">
         <div className="prog-box">
           <img
@@ -12,8 +12,70 @@ const Programs = () => {
         "
             alt=""
           />
-          <h2>Pre-college education</h2>
-          <p>Broaden the creative horizons of your kids</p>
+          <div className="text">
+            <h2>Pre-college education</h2>
+            <p>Broaden the creative horizons of your kids</p>
+          </div>
+        </div>
+        <div className="prog-box">
+          <img
+            src="
+        	https://magic-education.netlify.app/static/media/prog-1.439a25790165c679d18e.png
+        "
+            alt=""
+          />
+          <div className="text">
+            <h2>Pre-college education</h2>
+            <p>Broaden the creative horizons of your kids</p>
+          </div>
+        </div>{" "}
+        <div className="prog-box">
+          <img
+            src="
+        	https://magic-education.netlify.app/static/media/prog-1.439a25790165c679d18e.png
+        "
+            alt=""
+          />
+          <div className="text">
+            <h2>Pre-college education</h2>
+            <p>Broaden the creative horizons of your kids</p>
+          </div>
+        </div>
+        <div className="prog-box">
+          <img
+            src="
+        	https://magic-education.netlify.app/static/media/prog-1.439a25790165c679d18e.png
+        "
+            alt=""
+          />
+          <div className="text">
+            <h2>Pre-college education</h2>
+            <p>Broaden the creative horizons of your kids</p>
+          </div>
+        </div>{" "}
+        <div className="prog-box">
+          <img
+            src="
+        	https://magic-education.netlify.app/static/media/prog-1.439a25790165c679d18e.png
+        "
+            alt=""
+          />
+          <div className="text">
+            <h2>Pre-college education</h2>
+            <p>Broaden the creative horizons of your kids</p>
+          </div>
+        </div>{" "}
+        <div className="prog-box">
+          <img
+            src="
+        	https://magic-education.netlify.app/static/media/prog-1.439a25790165c679d18e.png
+        "
+            alt=""
+          />
+          <div className="text">
+            <h2>Pre-college education</h2>
+            <p>Broaden the creative horizons of your kids</p>
+          </div>
         </div>
       </div>
     </div>
