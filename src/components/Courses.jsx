167d0d64ab048bc5be975929d26a5b5ca6924431
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-26 21:41:05 +0500

    courses component end

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Courses = () => {
   return (
     <div>
+      <h1 className="courses-title">Courses</h1>
       <div className="course-boxes">
         <div className="card-1">
           <img
@@ -17,12 +18,42 @@ const Courses = () => {
             ex totam
           </p>
           <hr />
-          <h2>
-            Time: 3 pm
-          </h2>
-          <h2>
-            Teacher:Ressie Rottman
-          </h2>
+          <h2>Time: 3 pm</h2>
+          <h2>Teacher:Ressie Rottman</h2>
+          <button>Join Now</button>
+        </div>
+        <div className="card-1">
+          <img
+            src="
+        		https://magic-education.netlify.app/static/media/course3.e1f2a142fb3848a8d109.png
+        "
+            alt=""
+          />
+          <h2>Web Design</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing eli Voluptate,
+            ex totam
+          </p>
+          <hr />
+          <h2>Time: 3 pm</h2>
+          <h2>Teacher:Ressie Rottman</h2>
+          <button>Join Now</button>
+        </div>
+        <div className="card-1">
+          <img
+            src="
+            https://magic-education.netlify.app/static/media/course2.75335926b2963b6a4813.png
+            "
+            alt=""
+          />
+          <h2>Web Design</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing eli Voluptate,
+            ex totam
+          </p>
+          <hr />
+          <h2>Time: 3 pm</h2>
+          <h2>Teacher:Ressie Rottman</h2>
           <button>Join Now</button>
         </div>
       </div>
