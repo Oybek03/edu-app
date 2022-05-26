6171ce2b24c7cf28438d0753e47af35e84ca1349
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-26 22:22:39 +0500

    teacher component end

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -3,8 +3,88 @@ import React from "react";
 const Teachers = () => {
   return (
     <div>
-      <h1>Our Teachers</h1>
+      <h1 className="teach-title">Our Teachers</h1>
       <div className="teach-div">
+        <div className="teach-card">
+          <img
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
+            alt="teach"
+          />
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
+        </div>
+        <div className="teach-card">
+          <img
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
+            alt="teach"
+          />
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
+        </div>{" "}
+        <div className="teach-card">
+          <img
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
+            alt="teach"
+          />
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
+        </div>{" "}
+        <div className="teach-card">
+          <img
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
+            alt="teach"
+          />
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
+        </div>
+        <div className="teach-card">
+          <img
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
+            alt="teach"
+          />
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
+        </div>{" "}
+        <div className="teach-card">
+          <img
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
+            alt="teach"
+          />
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
+        </div>{" "}
+        <div className="teach-card">
+          <img
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
+            alt="teach"
+          />
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
+        </div>{" "}
+        <div className="teach-card">
+          <img
+            src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
+            alt="teach"
+          />
+          <div>
+            <h2>Vevlet Vachon</h2>
+            <h3>Design Head</h3>
+          </div>
+        </div>{" "}
         <div className="teach-card">
           <img
             src="https://magic-education.netlify.app/static/media/Teacher1.a2b74f0ccc3dc95cd4eb.png"
